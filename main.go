ef4214261faa75c20a131ecc2606b57b7caef0f1
behnamou <bihnam998@gmail.com>
2021-11-11 03:09:04 +0330

    test3

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func homepage(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
 	var member Member
 	err = json.Unmarshal(reqBody, &member)
 	if err != nil {
@@ -25,8 +34,31 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//insert member to database
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://behnamou:Behnam-2384@cluster0.u2qxk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-	//respnse 200 to writer
+	err = client.Connect(ctx)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer client.Disconnect(ctx)
+
+	memberCollection := client.Database("test").Collection("Member")
+
+	res, err := memberCollection.InsertOne(ctx, member)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	member.ID = res.InsertedID.(primitive.ObjectID)
+
+	//response 200 to writer
+
+	w.WriteHeader(http.StatusOK)
 
 }
 
@@ -43,6 +75,7 @@ func main() {
 }
 
 type Member struct {
+	ID       primitive.ObjectID `bson:"id"`
 	Username string
 	Name     string
 	Lastname string
