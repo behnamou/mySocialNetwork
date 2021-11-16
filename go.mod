34b5b33a4c03a78e5ae7f138753ed3ad7958f457
behnamou <bihnam998@gmail.com>
2021-11-16 13:36:24 +0330

    test goland

--- go.mod
+++ go.mod
@@ -13,7 +13,10 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/gofrs/uuid v4.1.0+incompatible // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/gomodule/redigo v1.8.5 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
--- main.go
+++ main.go
@@ -12,13 +12,18 @@ import (
 	"net/smtp"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/gomodule/redigo/redis"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/kavenegar/kavenegar-go"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/crypto/bcrypt"
 )
+
+var cache redis.Conn
 
 func homepage(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -155,17 +160,91 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/v1/auth/register", homepage).Methods(http.MethodPost)
 
+	myRouter.HandleFunc("/login", signin).Methods(http.MethodPost)
+
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
+func signin(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var logedInMember Member
+
+	enteredPassword, err := HashPassword(creds.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// 	coonect database
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://behnamou:Behnam-2384@cluster0.u2qxk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = client.Connect(ctx)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer client.Disconnect(ctx)
+
+	memberCollection := client.Database("test").Collection("Member")
+
+	if err = memberCollection.FindOne(ctx, bson.M{}).Decode(&logedInMember); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if enteredPassword != logedInMember.Password {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	// sessionToken := uuid.NewRandom().String()
+	sessionToken := uuid.New().String()
+
+	_, err = cache.Do("SETEX", sessionToken, "300", creds.Username)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "session_token",
+		Value:   sessionToken,
+		Expires: time.Now().Add(300 * time.Second),
+	})
+
+}
+
 func main() {
+	initCache()
 	handleRequests()
 	fmt.Println("hello")
 }
 
-// func mobileValidation(f1 validator.FieldLevel) bool {
-// 	return false
-// }
+func initCache() {
+	conn, err := redis.DialURL("redis://localhost")
+	if err != nil {
+		panic(err)
+	}
+	cache = conn
+}
+
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
 
 type Member struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
